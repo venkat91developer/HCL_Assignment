3126c5bd60cf9fab0498979ef0ca7524685f84cb
Venkatesh <venkatesh.m@vinsinfo.com>
2025-02-26 20:55:04 +0530

    Update - App Route Screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProgramComponent } from './Component/program/program.component';
-import { ParticipantComponent } from './Component/Participant/participant.component';
 import { LoginComponent } from './Component/Auth/login/login.component';
 import { ForgetPasswordComponent } from './Component/Auth/forget-password/forget-password.component';
 import { RegisterComponent } from './Component/Auth/register/register.component';
@@ -17,14 +15,5 @@ export const routes: Routes = [
     {
         path:'\forget-password',
         component:ForgetPasswordComponent
-    },
-    
-    {
-        path:'\program',
-        component:ProgramComponent
-    },
-    {
-        path:'\participant',
-        component:ParticipantComponent
     },
 ];
