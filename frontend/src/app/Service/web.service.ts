56f66b92163ec835309c124811fdd315ab7df892
Venkatesh <venkatesh.m@vinsinfo.com>
2025-02-26 19:38:12 +0530

    Update - Web Service Environment

--- frontend/src/app/Service/web.service.ts
+++ frontend/src/app/Service/web.service.ts
@@ -1,17 +1,38 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { env } from '../../environment/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
-    private apiUrl = 'http://localhost:3000/api/v1/';
+    private apiUrl = env.NODE_URL;
   
     constructor(private http: HttpClient) {}
   
     addProgram(data: FormData): Observable<any> {
-      return this.http.post(`${this.apiUrl}programs/add`, data);
+      return this.http.post(`${this.apiUrl}programs`, data);
+    }
+    getProgram(): Observable<any> {
+      return this.http.get(`${this.apiUrl}programs`);
+    }
+    updateProgram(data:FormData,id:any): Observable<any> {
+      return this.http.put(`${this.apiUrl}programs/${id}`, data);
+    }
+    deleteProgram(id:any): Observable<any> {
+      return this.http.delete(`${this.apiUrl}programs/${id}`);
+    }
+    addParticipant(data: FormData): Observable<any> {
+      return this.http.post(`${this.apiUrl}programs/participant`, data);
+    }
+    getParticipant(): Observable<any> {
+      return this.http.get(`${this.apiUrl}programs/participant`);
+    }
+    updateParticipant(data:FormData,id:any): Observable<any> {
+      return this.http.put(`${this.apiUrl}programs/participant/${id}`, data);
+    }
+    deleteParticipant(id:any): Observable<any> {
+      return this.http.delete(`${this.apiUrl}programs/${id}`);
     }
 }
 
---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,4 @@
+export const env = {
+    NODE_URL: "http://localhost:3000/api/v1/",
+    JWT_TOKEN: ""
+}
